93d0a373b5b39d16e2fd7e169d236f5aad3e8070
randall murphy <rmurphy@perseverenow.org>
2025-08-13 13:48:52 -0500

    update emailjs info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,9 +33,9 @@ const Contact = () => {
         'template_qaf5k0l', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Randall Murphy', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'rmurphy@perseverenow.org', //put your email here.
           message: form.message,
         },
         'sj7B23g4uy2J23Ir1' //paste your Public Key here. You'll get it in your profile section.
