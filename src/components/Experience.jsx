bf4565c0b066a91b572f3ac9bf0de81294d7ff26
randall murphy <rmurphy@perseverenow.org>
2025-08-21 14:15:00 -0500

    base64 fix for resume data top

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,7 +9,9 @@ import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { download, downloadHover, resume } from '../assets';
 import { textVariant } from '../utils/motion';
-import ramResume from '../assets/personal/Randall_Murphy_Resume.pdf'
+import ramResume from '../assets/personal/Randall_Murphy_Resume.pdf';
+import { useState } from 'react';
+
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
     contentStyle={{
@@ -52,6 +54,8 @@ const ExperienceCard = ({ experience }) => (
 );
 
 const Experience = () => {
+  const [iconSrc, setIconSrc] = useState(download);
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -68,6 +72,8 @@ const Experience = () => {
           {experiences.map((experience, index) => (
             <ExperienceCard key={index} experience={experience} />
           ))}
+
+          {/* Resume Element */}
           <VerticalTimelineElement
             contentStyle={{
               background: '#eaeaec',
@@ -91,7 +97,10 @@ const Experience = () => {
                 />
               </div>
             }>
-            <button
+            <a
+              href={ramResume}
+              target="_blank"
+              rel="noopener noreferrer"
               className="live-demo flex justify-between 
               sm:text-[18px] text-[14px] text-timberWolf 
               font-bold font-beckman items-center py-5 pl-3 pr-3 
@@ -100,30 +109,17 @@ const Experience = () => {
               sm:mt-[22px] mt-[16px] hover:bg-battleGray 
               hover:text-eerieBlack transition duration-[0.2s] 
               ease-in-out"
-              onClick={() =>
-                window.open(
-                  ramResume, //paste the link to your resume here
-                  '_blank'
-                )
-              }
-              onMouseOver={() => {
-                document
-                  .querySelector('.download-btn')
-                  .setAttribute('src', downloadHover);
-              }}
-              onMouseOut={() => {
-                document
-                  .querySelector('.download-btn')
-                  .setAttribute('src', download);
-              }}>
+              onMouseEnter={() => setIconSrc(downloadHover)}
+              onMouseLeave={() => setIconSrc(download)}
+            >
               MY RESUME
               <img
-                src={download}
+                src={iconSrc}
                 alt="download"
-                className="download-btn sm:w-[26px] sm:h-[26px] 
+                className="sm:w-[26px] sm:h-[26px] 
                 w-[23px] h-[23px] object-contain"
               />
-            </button>
+            </a>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
